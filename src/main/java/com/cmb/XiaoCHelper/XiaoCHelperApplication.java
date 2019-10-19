f48ab56bbab984b8cf2de634045a4e97b0e27dce
shiping998 <908079684@qq.com>
2019-10-19 09:58:12 +0800

    新建工程

---
+++ src/main/java/com/cmb/XiaoCHelper/XiaoCHelperApplication.java
@@ -0,0 +1,13 @@
+package com.cmb.XiaoCHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaoCHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaoCHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmb/XiaoCHelper/XiaoCHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmb.XiaoCHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoCHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
