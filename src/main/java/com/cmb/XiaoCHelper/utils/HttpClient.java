cc07b0669b0cca65c90ae27a71576b5bf6fd4fe1
Amaterasu_Z <NerverSurrender666@163.com>
2019-10-19 15:27:43 +0800

    调用文本分析服务测试成功

--- src/main/java/com/cmb/XiaoCHelper/utils/HttpClient.java
+++ src/main/java/com/cmb/XiaoCHelper/utils/HttpClient.java
@@ -4,6 +4,8 @@ import org.springframework.http.*;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 public class HttpClient {
 
      /**
@@ -26,6 +28,26 @@ public class HttpClient {
         ResponseEntity<String> response = client.exchange(url, method, requestEntity,String .class);
 
         return response.getBody();
+    }
+
+    /**
+     * 发送JSON请求
+     * @param url
+     * @param params
+     * @return
+     */
+    public static String sendJSONPostRequest(String url, Map<String, String> params){
+        RestTemplate client = new RestTemplate();
+        //新建Http头，add方法可以添加参数
+        HttpHeaders headers = new HttpHeaders();
+        // 以JSON的方式提交
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        //将请求头部和参数合成一个请求
+        HttpEntity<Map<String, String>> requestEntity = new HttpEntity<>(params, headers);
+        //执行HTTP请求，将返回的结构使用String 类格式化（可设置为对应返回值格式的类）
+        ResponseEntity<String> resEntity = client.postForEntity(url, requestEntity, String.class);
+
+        return resEntity.getBody();
     }
 
     /**
--- src/test/java/com/cmb/XiaoCHelper/unitTest.java
+++ src/test/java/com/cmb/XiaoCHelper/unitTest.java
@@ -2,8 +2,18 @@ package com.cmb.XiaoCHelper;
 
 import com.baidu.aip.speech.AipSpeech;
 import com.cmb.XiaoCHelper.baiduAIP.SingleAipSpeech;
+import com.cmb.XiaoCHelper.utils.HttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class unitTest {
 
@@ -23,5 +33,28 @@ public class unitTest {
 
     }
 
+    @Test
+    public void sendAndRec(){
+        AipSpeech client = SingleAipSpeech.getInstance();
+        String path = "C:\\计科学习\\JavaPrj\\xiaochelper\\zy-2901ms-16kbps-16000hz.wav";
+        // 调用接口
+        JSONObject res = client.asr(path, "wav", 16000, null);
+        System.out.println(res);
 
+        try {
+            JSONArray arr = (JSONArray)res.get("result");
+            System.out.println(arr.get(0));
+            Map<String, String> map = new HashMap<>();
+            String text = (String)arr.get(0);
+            text = text.substring(0, 6);
+//            System.out.println(text);
+            map.put("text", text);
+            String s = HttpClient.sendJSONPostRequest("http://99.15.215.14:8080/url", map);
+            System.out.println(s);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
